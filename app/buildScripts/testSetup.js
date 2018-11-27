a162985fce93a6a1049b7f5d543332a5ec49cd15
ayilmaz <stfal1@gmail.com>
2018-11-27 14:30:03 -0500

    Updated Babel register.

--- app/buildScripts/testSetup.js
+++ app/buildScripts/testSetup.js
@@ -1,7 +1,7 @@
 // This file is not transpiled, so must use CommonJs and ES5
 
 // Register babel to transpile before our tests run
-require('babel-register')();
+require('@babel/register')();
 
 // Disable webpack features that Mocha does not understand
 require.extensions['.css'] = function() {};
